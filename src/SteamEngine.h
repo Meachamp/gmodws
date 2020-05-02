79024201ef562c1470ddb1fac05b800927a13840
Meachamp <34523297+Meachamp@users.noreply.github.com>
2020-05-02 03:53:14 -0400

    Update steam engine header

--- src/SteamEngine.h
+++ src/SteamEngine.h
@@ -27,6 +27,37 @@ public:
   virtual int GetIClientGameServerStats(int,int,char const*);
   virtual int GetIClientNetworking(int,int,char const*);
   virtual void* GetIClientRemoteStorage(int,int,char const*);
+  virtual void GetIClientScreenshots(int,int);
+  virtual void SetWarningMessageHook(void*);
+  virtual void GetIClientGameCoordinator(int,int);
+  virtual void SetOverlayNotificationPosition(int);
+  virtual void SetOverlayNotificationInset(int,int);
+  virtual void HookScreenshots(bool);
+  virtual void IsScreenshotsHooked(void);
+  virtual void IsOverlayEnabled(void);
+  virtual void GetAPICallResult(int,unsigned long long,void *,int,int,bool *);
+  virtual void GetIClientProductBuilder(int,int);
+  virtual void GetIClientDepotBuilder(int,int);
+  virtual void GetIClientNetworkDeviceManager(int);
+  virtual void ConCommandInit(void*);
+  virtual void GetIClientAppManager(int,int);
+  virtual void GetIClientConfigStore(int,int);
+  virtual void BOverlayNeedsPresent(void);
+  virtual void GetIClientGameStats(int,int);
+  virtual void GetIClientHTTP(int,int);
+  virtual void FlushBeforeValidate(void);
+  virtual void BShutdownIfAllPipesClosed(void);
+  virtual void GetIClientAudio(int,int);
+  virtual void GetIClientMusic(int,int);
+  virtual void GetIClientUnifiedMessages(int,int);
+  virtual void GetIClientController(int);
+  virtual void GetIClientParentalSettings(int,int);
+  virtual void GetIClientStreamLauncher(int,int);
+  virtual void GetIClientDeviceAuth(int,int);
+  virtual void GetIClientRemoteClientManager(int);
+  virtual void GetIClientStreamClient(int,int);
+  virtual void GetIClientShortcuts(int,int);
+  virtual void* GetIClientUGC(int,int);
 };
 
 
